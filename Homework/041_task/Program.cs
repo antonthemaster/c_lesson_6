314d714f1c0d2fe3b6aa5c0b76349d2185a3122b
Folky <thekiralog@ya.ru>
2023-05-31 19:38:08 +0400

    Initial commit

---
+++ Homework/041_task/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Enter a quantity of nums: ");
+int num_length = Convert.ToInt32(Console.ReadLine());
+int result = new int();
+for (int i=0; i < num_length; i++){
+    Console.Write($"You entered {i} numbers of {num_length}, now enter {i+1} number: ");
+    int user_number = Convert.ToInt32(Console.ReadLine());
+    if (user_number > 0)
+    {
+        result++;
+    }
+}
+Console.WriteLine();
+Console.WriteLine($"Amount of numbers over 0: {result}");
---
+++ Homework/043_task/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Enter a value of k1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter a value of b1: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter a value of k2: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter a value of b2: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+
+double x = -(k1 - k2)/(b1-b2);
+double y = b1 * x + k1;
+
+
+Console.WriteLine($"({x},{y})");
+
+
+
